50ec259a3604e2667f76d04886954acb7e1fabd8
Gavin Dsouza <gavin18d@gmail.com>
2019-04-18 13:43:30 +0530

    [ADDED] Database class + refactored everything phew [UPDATED] gitignore

--- desktop-client/desktop-client.py
+++ desktop-client/desktop-client.py
@@ -1,8 +1,9 @@
 # imports - standard imports
 import tkinter as tk
-import sqlite3 as sq
+import sqlite3
 import json
 import os
+import ntpath
 
 # imports - third party imports
 from PIL import ImageTk, Image
@@ -10,228 +11,246 @@ import requests
 from argon2 import PasswordHasher
 
 
+supported_img_list = [
+    "ase",
+    "art",
+    "bmp",
+    "blp",
+    "cd5",
+    "cit",
+    "cpt",
+    "cr2",
+    "cut",
+    "dds",
+    "dib",
+    "djvu",
+    "egt",
+    "exif",
+    "gif",
+    "gpl",
+    "grf",
+    "icns",
+    "ico",
+    "iff",
+    "jng",
+    "jpeg",
+    "jpg",
+    "jfif",
+    "jp2",
+    "jps",
+    "lbm",
+    "max",
+    "miff",
+    "mng",
+    "msp",
+    "nitf",
+    "ota",
+    "pbm",
+    "pc1",
+    "pc2",
+    "pc3",
+    "pcf",
+    "pcx",
+    "pdn",
+    "pgm",
+    "PI1",
+    "PI2",
+    "PI3",
+    "pict",
+    "pct",
+    "pnm",
+    "pns",
+    "ppm",
+    "psb",
+    "psd",
+    "pdd",
+    "psp",
+    "px",
+    "pxm",
+    "pxr",
+    "qfx",
+    "raw",
+    "rle",
+    "sct",
+    "sgi",
+    "rgb",
+    "int",
+    "bw",
+    "tga",
+    "tiff",
+    "tif",
+    "vtf",
+    "xbm",
+    "xcf",
+    "xpm",
+    "3dv",
+    "amf",
+    "ai",
+    "awg",
+    "cgm",
+    "cdr",
+    "cmx",
+    "dxf",
+    "e2d",
+    "egt",
+    "eps",
+    "fs",
+    "gbr",
+    "odg",
+    "svg",
+    "stl",
+    "vrml",
+    "x3d",
+    "sxd",
+    "v2d",
+    "vnd",
+    "wmf",
+    "emf",
+    "art",
+    "xar",
+    "png",
+    "webp",
+    "jxr",
+    "hdp",
+    "wdp",
+    "cur",
+    "ecw",
+    "iff",
+    "lbm",
+    "liff",
+    "nrrd",
+    "pam",
+    "pcx",
+    "pgf",
+    "sgi",
+    "rgb",
+    "rgba",
+    "bw",
+    "int",
+    "inta",
+    "sid",
+    "ras",
+    "sun",
+    "tga"
+]
 hasher = PasswordHasher()
 
 
+class Database(object):
+    DB_LOCATION = 'client.db'
+
+    def __init__(self):
+        self.connection = sqlite3.connect(Database.DB_LOCATION)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS user_credentials (Username TEXT, Password TEXT, Token TEXT)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS img_path (Image TEXT)")
+        self.commit()
+
+    def commit(self):
+        self.connection.commit()
+
+    def get_user_credentials(self):
+        self.cursor.execute("SELECT * FROM user_credentials")
+        username, password, token = self.cursor.fetchall()
+
+        return username, password, token
+
+    def add_user_credentials(self, user_name, password, token):
+        self.cursor.execute("INSERT INTO user_credentials (Username, Password, Token) VALUES (?, ?, ?)",
+                            (user_name, hasher.hash(password), hasher.hash(token)))
+        self.commit()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        self.cursor.close()
+        if isinstance(exc_value, Exception):
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+        self.connection.close()
+
+
+def check_server_ip_address(address):
+    check_if_server = requests.get("http://"+address+":5000/hello")
+
+    if check_if_server.status_code == 200:
+        print("Server exists!")
+        return True
+
+    else:
+        print("Server is not okay")
+        return False
+
+
+def receive_token(username, password, address):
+    payload = {
+        'username': username,
+        'password': password
+    }
+
+    receive_token = requests.post("http://"+address, data=payload)
+
+    print(receive_token.json())
+    token_from_server = json.loads(receive_token)
+
+    return token_from_server
+    
+
+
+def gen_img_list(path, *args):
+    img_path_list = []
+
+    if path != '':
+        path = path
+    else:
+        path = args
+        try:
+            for object in os.listdir(path):
+                if os.path.isdir(path + '/' + object):
+                    gen_img_list(path + '/' + object)
+                elif os.path.isfile(path + '/' + object):
+                    ext = object.split('.')[-1]
+                    if ext in supported_img_list:
+                        img_path_list.append('{0}/{1}'.format(path, object))
+
+        # except NotADirectoryError or FileNotFoundError or OSError or PermissionError:
+        except Exception:
+            pass
+
+    return img_path_list
+
+
+def send_pictures(token, address, img_list):
+    total_num = len(img_list)
+    
+    for curr_num, img_path in enumerate(img_list):
+        data = {
+            'path': img_path,
+            'file_name' : ntpath.basename(img_path),
+            'media': open(img_path, 'rb'),
+            'token': token,
+            'enctype': 'multipart/form-data'
+        }
+        print(data)
+        send_image = requests.post(
+            "http://"+address+":5000/api/v1/upload", data=data)
+
+
+def send_token(token, address):
+    payload = {
+        'token': token
+    }
+    send_token = requests.post("http://"+address, data=payload)
+    print(token)
+
+
 def main():
-
-    conn = sq.connect('client.db')
-    connection = conn.cursor()
-
     root = tk.Tk()
     root.title("Desktop-Client")
     root.geometry("600x600")
-
-    ######### IP ADDRESS OF SERVER ################
-
-    def check_server_ip_address(address):
-        print("Reached here ", address)
-        print("http://"+address+":5000/hello")
-        check_if_server = requests.get("http://"+address+":5000/hello")
-        
-        if check_if_server.status_code == 200:
-            # if 1 == 1:
-            print("Server is okay ")
-
-            # CheckBox Image
-            canvas = tk.Canvas(main_ip_frame)
-            image = Image.open("utils/checkmark.png")
-            image = image.resize((20, 20))
-            canvas.img = ImageTk.PhotoImage(image)
-            canvas.create_image(10, 10, image=canvas.img)
-            canvas.pack(side=tk.RIGHT)
-
-            check_credentials['state'] = 'normal'
-            check_token['state'] = 'normal'
-
-        else:
-            print("Server is not okay")
-
-    def send_credentials(username, password, address):
-        payload = {
-            'username': username,
-            'password': password
-        }
-        # receive_token= requests.post("http://"+address, data= payload)
-        # print(receive_token.json())
-
-        # token_from_server= json.loads(receive_token)
-        token = "aaaa"
-        print(username, password, token)
-        connection.execute(
-            "CREATE TABLE IF NOT EXISTS user_credentials (Username TEXT, Password TEXT, Token TEXT)")
-        connection.execute('INSERT INTO user_credentials (Username, Password, Token) VALUES (?, ?, ?)',
-                           (username, ph.hash(password), ph.hash(token)))
-        conn.commit()
-
-        send_pictures(token, address)
-
-    def send_pictures(token, address):
-
-        image = [
-            "ase",
-            "art",
-            "bmp",
-            "blp",
-            "cd5",
-            "cit",
-            "cpt",
-            "cr2",
-            "cut",
-            "dds",
-            "dib",
-            "djvu",
-            "egt",
-            "exif",
-            "gif",
-            "gpl",
-            "grf",
-            "icns",
-            "ico",
-            "iff",
-            "jng",
-            "jpeg",
-            "jpg",
-            "jfif",
-            "jp2",
-            "jps",
-            "lbm",
-            "max",
-            "miff",
-            "mng",
-            "msp",
-            "nitf",
-            "ota",
-            "pbm",
-            "pc1",
-            "pc2",
-            "pc3",
-            "pcf",
-            "pcx",
-            "pdn",
-            "pgm",
-            "PI1",
-            "PI2",
-            "PI3",
-            "pict",
-            "pct",
-            "pnm",
-            "pns",
-            "ppm",
-            "psb",
-            "psd",
-            "pdd",
-            "psp",
-            "px",
-            "pxm",
-            "pxr",
-            "qfx",
-            "raw",
-            "rle",
-            "sct",
-            "sgi",
-            "rgb",
-            "int",
-            "bw",
-            "tga",
-            "tiff",
-            "tif",
-            "vtf",
-            "xbm",
-            "xcf",
-            "xpm",
-            "3dv",
-            "amf",
-            "ai",
-            "awg",
-            "cgm",
-            "cdr",
-            "cmx",
-            "dxf",
-            "e2d",
-            "egt",
-            "eps",
-            "fs",
-            "gbr",
-            "odg",
-            "svg",
-            "stl",
-            "vrml",
-            "x3d",
-            "sxd",
-            "v2d",
-            "vnd",
-            "wmf",
-            "emf",
-            "art",
-            "xar",
-            "png",
-            "webp",
-            "jxr",
-            "hdp",
-            "wdp",
-            "cur",
-            "ecw",
-            "iff",
-            "lbm",
-            "liff",
-            "nrrd",
-            "pam",
-            "pcx",
-            "pgf",
-            "sgi",
-            "rgb",
-            "rgba",
-            "bw",
-            "int",
-            "inta",
-            "sid",
-            "ras",
-            "sun",
-            "tga"
-        ]
-
-        img = []
-
-        def tree(path, *args):
-            if path != '':
-                path = path
-            else:
-                path = args
-            try:
-                for object in os.listdir(path):
-                    if os.path.isdir(path + '/' + object):
-                        tree(path + '/' + object)
-                    elif os.path.isfile(path + '/' + object):
-                        ext = object.split('.')[-1]
-                        if ext in image:
-                            img.append('{0}/{1}'.format(path, object))
-
-            # except NotADirectoryError or FileNotFoundError or OSError or PermissionError:
-            except Exception as e:
-                pass
-
-        path = "/home/aarish/aarish/mh2019"
-        tree(path)
-        print(img)
-
-        for path in img:
-            data = {
-                'path': path,
-                'media': open(path, 'rb'),
-                'token': token,
-                'enctype': 'multipart/form-data'
-            }
-            print(data)
-            send_image = requests.post(
-                "http://"+address+":5000/api/v1/upload", data=data)
-
-    def send_token(token):
-        payload = {
-            'token': token
-        }
-        # send_token= requests.post("http://"+address, data= payload)
-        print(token)
 
     main_ip_frame = tk.Frame()
     ip_frame = tk.Frame(main_ip_frame)
@@ -294,18 +313,34 @@ def main():
           "\nIP Address ", ip.get(), "\nToken ", token.get())
 
 
+
 if __name__ == '__main__':
-    conn = sq.connect('client.db')
-    connection = conn.cursor()
+    path = "/home/gavin"
+    img = gen_img_list(path)
+    print(img)
 
-    connection.execute("SELECT * FROM user_credentials")
-    conn.commit()
+    # Set up the database for credentials
+    with Database() as db:
+        db.commit()
 
-    data = connection.fetchall()
 
-    # if data:
-    #     print("Not Empty")
+    # Set up the database for img_path_list
 
-    # else:
-    print("Empty")
+    # Check connection to server
+    #   input address
+    #   if exists:
+    #
+        # # CheckBox Image
+        # canvas = tk.Canvas(main_ip_frame)
+        # image = Image.open("utils/checkmark.png")
+        # image = image.resize((20, 20))
+        # canvas.img = ImageTk.PhotoImage(image)
+        # canvas.create_image(10, 10, image=canvas.img)
+        # canvas.pack(side=tk.RIGHT)
+
+        # check_credentials['state'] = 'normal'
+        # check_token['state'] = 'normal'
+    # else
+    # dont
+
     main()
